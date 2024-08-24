9979c85f562f40b1464f4865f3dd5d1e729ff370
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-08-25 01:07:26 +0600

    pull

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
    
     domains: ['localhost','estarch.com.bd', 'res.cloudinary.com','fabrilife.com','api.v1.estarch.online','http//:localhost:5000'],
   },
-  unoptimized: true,
 
 };
