75b8045c40e0759b373e4b28d99e96f2cd552d34
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-09-08 03:10:19 +0600

    New arrival all products card change

--- components/NewArrivalAllProducts/NewArrivalAllProducts.js
+++ components/NewArrivalAllProducts/NewArrivalAllProducts.js
@@ -34,7 +34,7 @@ const NewArrivalAllProducts = () => {
     useEffect(() => {
         const fetchProducts = async () => {
             let url = `${baseUrl}/api/products/new-all-products`;
-          
+
             // Add ranges to the query string if there are selected ranges
             if (selectedRanges.length > 0) {
                 const rangesQuery = JSON.stringify(selectedRanges);
@@ -84,7 +84,10 @@ const NewArrivalAllProducts = () => {
     const handleSortChange = (e) => {
         setSortBy(e.target.value);
     };
-
+    
+    const truncateText = (text, maxLength) => {
+        return text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
+      };
     const handleCheckboxChangeCat = (subcategory) => {
         setSelectedSubcategories((prevSubcategories) =>
             prevSubcategories.includes(subcategory)
@@ -159,8 +162,8 @@ const NewArrivalAllProducts = () => {
                 </label>
             </div>
 
-           {/* filter button */}
-           <div className="flex gap-3">
+            {/* filter button */}
+            <div className="flex gap-3">
                 <label
                     htmlFor="my-drawer-2"
                     className="btn btn-sm drawer-button lg:hidden mb-4"
@@ -201,52 +204,50 @@ const NewArrivalAllProducts = () => {
                         {products.slice(0, index).map((product) => (
                             <div
                                 key={product._id}
-                                className="card card-compact bg-base-200 shadow-lg rounded-none h-[340px] md:h-[490px] relative border-2 border-base-200 hover:border-blue-300"
+                                className="card card-compact bg-base-200 shadow-lg rounded-none relative border-2 border-base-200 hover:border-blue-300"
                             >
                                 <Link href={`/product/${product?.productName}?sku=${product?.SKU}`}>
-                                <figure>
-                                    <Image sizes="30vw" src={`${baseUrl}/${product.images[0]}`} alt={product.productName} width={350}
-                                        height={400} />
-                                </figure>
-                                <div className="pt-1 lg:px-6 px-2">
-                                    <h2 className="md:text-[17px] text-[14px] font-bold text-center">
-                                        {product.productName.length > 25
-                                            ? `${product.productName.slice(0, 25)}...`
-                                            : product.productName
-                                        }</h2>
-                                    <div className='text-center'>
-                                        <div className="absolute bottom-8 md:bottom-10 left-8 md:left-[85px]">
-                                            <p className={`bg-black text-white text-sm md:text-[16px] mt-2 w-full mx-auto  px-2 ${product.regularPrice - product.salePrice > 0 ? 'visible' : 'invisible'}`}>
-                                                Save Tk. {product.regularPrice - product.salePrice}
-                                            </p>
-                                            {
-                                                product.regularPrice - product.salePrice > 0 && (
-                                                    <p className='my-1 text-[16px] md:text-[20px] text-black text-center '>
-                                                        <span>TK.</span>{product.salePrice}
-                                                        <span className='md:text-[17px] text-sm line-through text-red-500'> Tk.{product.regularPrice}</span>
-                                                    </p>
-                                                )
-                                            } 
-                                        </div>
+                                    <figure>
+                                        <Image sizes="30vw" src={`${baseUrl}/${product.images[0]}`} alt={product.productName} width={350}
+                                            height={400} />
+                                    </figure>
+                                    <div className="pt-1 lg:px-6 px-2">
+                                        <h2 className="md:text-[14px] text-[14px] font-bold text-center whitespace-nowrap overflow-hidden text-ellipsis">
+                                            {truncateText(product.productName, product.productName.length)}
+                                        </h2>
+                                        <div className='text-center'>
+                                            <div className="">
+                                                <p className={`bg-black text-white text-sm md:text-[14px] mt-2 md:mx-8 mx-4 ${product.regularPrice - product.salePrice > 0 ? 'visible' : 'invisible'}`}>
+                                                    Save Tk. {product.regularPrice - product.salePrice}
+                                                </p>
+                                                {
+                                                    product.regularPrice - product.salePrice > 0 && (
+                                                        <p className='my-1 text-[16px] md:text-[20px] text-black text-center '>
+                                                            <span>TK.</span>{product.salePrice}
+                                                            <span className='md:text-[17px] text-sm line-through text-red-500'> Tk.{product.regularPrice}</span>
+                                                        </p>
+                                                    )
+                                                }
+                                            </div>
 
-                                        {product.regularPrice - product.salePrice <= 0 && (
-                                            <p className='my-1 text-[17px] md:text-[20px] text-black text-center absolute bottom-8 md:bottom-10 left-14 md:left-[110px]'>
-                                                <span className=''>TK.</span>{product.salePrice}
-                                            </p>
-                                        )}
+                                            {product.regularPrice - product.salePrice <= 0 && (
+                                                <p className='my-1 text-[17px] md:text-[20px] text-black text-center bottom-8 md:bottom-10 left-14 md:left-[110px]'>
+                                                    <span className=''>TK.</span>{product.salePrice}
+                                                </p>
+                                            )}
+                                        </div>
                                     </div>
-                                </div>
                                 </Link>
-                                <div className='text-center shadow-lg absolute w-full bottom-0'>
-                                    
-                                        <button onClick={() => dispatch(openProductModal(product))} className=" bg-[#1E201E] text-white w-full md:py-2 py-1">BUY NOW</button>
-                                  
+                                <div className='text-center shadow-lg  w-full bottom-0'>
+
+                                    <button onClick={() => dispatch(openProductModal(product))} className=" bg-[#1E201E] text-white w-full md:py-2 py-1">BUY NOW</button>
+
                                 </div>
                             </div>
                         ))}
                         <div className="place-self-center md:col-span-4 col-span-2 ">
                             <button onClick={() => setIndex(index + 20)} className={`btn flex items-center gap-1 btn-sm btn-primary text-white ${products.length <= index ? "hidden" : 'grid'}`}>
-                                SEE MORE 
+                                SEE MORE
                             </button>
                         </div>
                     </div>
